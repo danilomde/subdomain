11d337a8656fc0dbd109de27ba9172e0a7933398
Danilo Zeni <danilomde@gmail.com>
2019-07-26 10:40:12 -0300

    Page home ok

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,6 @@
-@extends('adminlte::page')
-
-@section('title', 'AdminLTE')
-
-@section('content_header')
-    <h1>Dashboard</h1>
-@stop
-
-@section('content')
-    <p>You are logged in!</p>
-@stop
+<center>
+<br /><h2>'.$escola->username.'.jumpercursos.com.br</h2><br /><br />
+<h4>$escola->email</h4>
+<br /><br />
+<img src="imagem.png" style="margin:auto;"/>
+</center>;
