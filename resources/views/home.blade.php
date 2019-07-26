f53bbe4a2f02059283c278b33d7149d858df28f1
Danilo Zeni <danilomde@gmail.com>
2019-07-26 10:51:10 -0300

    Page home ok6

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,6 @@
 <center>
-<br /><h2>'.{{$escola->username}}.'.jumpercursos.com.br</h2><br /><br />
+<br /><h2>.{{$escola->username}}.jumpercursos.com.br</h2><br /><br />
 <h4>{{$escola->email}}</h4>
 <br /><br />
 <img src="imagem.png" style="margin:auto;"/>
 </center>;
-  
